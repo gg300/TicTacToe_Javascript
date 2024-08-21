49facea84663efe4177f144c7a123eb281fa8568
gg300 <georgebot300@yahoo.com>
2024-08-21 13:02:28 +0300

    created ids for every div

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ function create_table(){
     const button = document.getElementById("Start");
     button.remove();
     for(let i=1;i<=9;i++){
-        const ele = document.createElement("div"+i);
+        const ele = document.createElement("div");
         ele.innerHTML= "Hello world";
         ele.style.background = "green";
         ele.className = "elem";
+        ele.id="div"+i;
         const bodi = document.getElementById("cont");
         bodi.appendChild(ele);
     }

7fec68bb28570890b965652dbfa55401cf690795
gg300 <georgebot300@yahoo.com>
2024-08-22 16:16:15 +0300

    created a kind of restriction for  X and O

--- App.js
+++ App.js
@@ -5,11 +5,12 @@ function create_table(){
     button.remove();
     for(let i=1;i<=9;i++){
         const ele = document.createElement("div");
-        ele.innerHTML= "Hello world";
+        ele.innerHTML= "";
         ele.style.background = "green";
         ele.className = "elem";
         ele.id=""+i;
         ele.onclick = function(){
+            if(ele.innerHTML == ""){
             if(k%2==0){
                 ele.innerHTML = "X";
                 k+=1;
@@ -17,7 +18,7 @@ function create_table(){
             else{
                 ele.innerHTML = "O";
                 k+=1;
-            }
+            }}
         };
         const bodi = document.getElementById("cont");
         bodi.appendChild(ele);
